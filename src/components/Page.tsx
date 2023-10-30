cc05731ff556fc6c65125ae956cf992c7a33078e
GSerdg <grebelsergey@mail.ru>
2023-10-30 23:57:05 +0300

    feat: add spinner

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,11 +3,13 @@ import { PeopleResult } from '../types/types';
 import Cards from './Cards';
 import Input from './Input';
 import ResponseServise from './api/responses';
+import { BallTriangle } from 'react-loader-spinner';
 
 interface PageProps {}
 
 interface PageState {
   respData: PeopleResult[];
+  spinner: boolean;
 }
 
 class Page extends React.Component<PageProps, PageState> {
@@ -15,16 +17,20 @@ class Page extends React.Component<PageProps, PageState> {
     super(props);
 
     this.handleInputSubmit = this.handleInputSubmit.bind(this);
-    this.state = { respData: [] };
+    this.state = { respData: [], spinner: false };
   }
 
   async getPeopleForName(name: string) {
+    this.setState({ spinner: true });
     const { results } = await ResponseServise.getForName(name);
+    this.setState({ spinner: false });
     this.setState({ respData: results });
   }
 
   async getPeoples() {
+    this.setState({ spinner: true });
     const { results } = await ResponseServise.getAll();
+    this.setState({ spinner: false });
     this.setState({ respData: results });
   }
 
@@ -39,11 +45,22 @@ class Page extends React.Component<PageProps, PageState> {
 
   render() {
     const data = this.state.respData;
-
+    let cards: JSX.Element | null = null;
+    if (!this.state.spinner) {
+      cards = <Cards respData={data} />;
+    }
     return (
       <div className="page">
         <Input onInputSubmit={this.handleInputSubmit} />
-        <Cards respData={data} />
+        <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#4fa94d"
+          ariaLabel="ball-triangle-loading"
+          visible={this.state.spinner}
+        />
+        {cards}
       </div>
     );
   }

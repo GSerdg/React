de0a6c9282d85cbaa54e48b5edd65974ca53a432
GSerdg <grebelsergey@mail.ru>
2023-11-21 16:42:19 +0300

    feat: create Next.js app

--- src/components/error-component/ErrorComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouteError } from 'react-router-dom';
-import NotFound from '../../pages/not-found/NotFound';
-
-export default function ErrorComponent() {
-  const error = useRouteError();
-  console.error(error);
-
-  return <NotFound />;
-}

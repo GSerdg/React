50251aae12de43f9615daf648288013533d12eb7
GSerdg <grebelsergey@mail.ru>
2023-11-26 18:27:41 +0300

    ifx: del

--- src/shared/useSelector.ts
+++
@@ -1,7 +0,0 @@
-import {
-  useSelector as useReduxSelector,
-  TypedUseSelectorHook,
-} from 'react-redux';
-import { RootState } from '../store/store';
-
-export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
